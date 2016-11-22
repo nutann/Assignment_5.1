f775de6946b654bd47cf12e1a61dfd9ab742429d
nutnaik <nutnaik@cisco.com>
2016-11-22 19:36:57 +0530

    assignment 5.1

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Nutan";
+var age = 24;
+var dob = new Date("10 April,1990");
+var pob = "honavar"
+
+console.log("Name : " +name);
+console.log("age : " +age);
+console.log("dob : " +dob);
+console.log("pob : " +pob);
